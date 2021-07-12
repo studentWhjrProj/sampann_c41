e0ff952038964bb74424848d5cf35cbd95084d77
studentWhjrProj <80413001+studentWhjrProj@users.noreply.github.com>
2021-07-12 16:24:48 +0530

    Add files via upload

---
+++ js/player.js
@@ -0,0 +1,39 @@
+class Player {
+    constructor() {
+        this.index = null;
+        this.distance = 0;
+        this.name = null;
+        this.score =0;
+    }
+
+    getCount() {
+        var playerCountRef = database.ref('playerCount');
+        playerCountRef.on("value", (data) => {
+            playerCount = data.val();
+        })
+    }
+
+    updateCount(count) {
+        database.ref('/').update({
+            playerCount: count
+        });
+    }
+
+    update() {
+        var playerIndex = "player/player" + this.index;
+        database.ref(playerIndex).set({
+            name: this.name,
+            distance: this.distance,
+            score:this.score
+        });
+    }
+
+    static getPlayerInfo() {
+        var playerInfoRef = database.ref('player');
+        playerInfoRef.on("value", (data) => {
+            allPlayers = data.val();
+        })
+    }
+
+    
+}
